d3ec99d2821c0189085d3284dea17cbcc0aed450
diper92 <diper92@icloud.com>
2022-11-06 03:03:25 +0700

    color blender

--- mixer rgb colors/ViewController.swift
+++ mixer rgb colors/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLable: UILabel!
+    @IBOutlet var greenLable: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 15
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        setColor()
     }
-
-
+    
+    @IBAction func rgbSliderAction(_ sender: UISlider) {
+        colorView.backgroundColor = UIColor(
+            red:CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            
+        )
+        setColor()
+    }
+    
+    private func setColor() {
+        redLable.text = String(format: "%.2f", redSlider.value)
+        greenLable.text = String(format: "%.2f",greenSlider.value)
+        blueLabel.text  = String(format: "%.2f", blueSlider.value)
+        
+    }
+    
 }
+
 
